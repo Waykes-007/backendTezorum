5e8513332e0f509e1380c9c24488f9a0dfe887f5
josueacuna123 <josueacuna380@gmail.com>
2026-05-17 03:11:31 -0500

    Conectar webhook Izipay con creacióndepedidos

--- src/routes/izipayRoutes.js
+++ src/routes/izipayRoutes.js
@@ -4,5 +4,10 @@ const { crearFormToken, webhook } = require('../controllers/izipayController');
 
 router.post('/crear-form-token', crearFormToken);
 router.post('/webhook',          webhook);
+router.get('/webhook', (req, res) => {
+  res.status(200).send('OK');
+});
+
+router.post('/webhook', webhook);
 
 module.exports = router;
