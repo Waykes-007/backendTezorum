206a540b969e99b7bff2d57a3c8de697554335f2
josueacuna123 <josueacuna380@gmail.com>
2026-07-08 20:29:43 -0500

    Ampliar limite de body a 10mb para subida de imagenes base64

--- src/app.js
+++ src/app.js
@@ -15,8 +15,8 @@ const app = express();
 
 // Middlewares
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // Rutas específicas
 app.use('/api/plan',        planRoutes);
